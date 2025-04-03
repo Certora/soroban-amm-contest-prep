db81e1e7ae0ddeb06e121d9a60e642e185f6b5f5
Chandrakana Nandi <chandra.nandi90@gmail.com>
2025-04-03 09:13:10 -0700

    chore: more setup work

--- fees_collector/src/certora_specs/fee_collector_rules.rs
+++ fees_collector/src/certora_specs/fee_collector_rules.rs
@@ -1,22 +1,23 @@
-use cvlr::clog;
 use soroban_sdk::Env;
 
-use cvlr::asserts::{cvlr_assert, cvlr_assume};
+use cvlr::asserts::cvlr_assert;
 use cvlr_soroban_derive::rule;
 use cvlr_soroban::nondet_address;
+use cvlr::clog;
 
 pub use crate::contract::FeesCollector;
-use access_control::access::AccessControl;
 use access_control::role::Role;
 use access_control::management::SingleAddressManagementTrait;
-use crate::interface::{AdminInterface, UpgradeableContract};
+use crate::interface::AdminInterface;
 
 use crate::certora_specs::ACCESS_CONTROL;
 
 #[rule]
 pub fn init_admin_sets_admin(e: Env) {
     let address = nondet_address();
-    let fee_collector = FeesCollector::init_admin(e, address.clone());
+    clog!(cvlr_soroban::Addr(&address));
+    FeesCollector::init_admin(e, address.clone());
     let addr = unsafe { ACCESS_CONTROL.clone().unwrap().get_role(&Role::Admin) };
+    clog!(cvlr_soroban::Addr(&addr));
     cvlr_assert!(addr == address);
 }
